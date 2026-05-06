d3710813c238f7c3820e836d6d4be796038f8b94
Venkatesh Agnihotri <venku.agni@gmail.com>
2026-05-06 21:51:42 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+             if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Integer> tCount = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            sCount.put(c, sCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            tCount.put(c, tCount.getOrDefault(c, 0) + 1);
+        }
+
+        return sCount.equals(tCount);
+    }
+}
